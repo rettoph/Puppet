08d82e8ebbb8be6be2a202a8bbe07e8a4562997c
Rettoph <turner.anthony.d@gmail.com>
2019-07-02 20:59:15 -0600

    Initial commit

---
+++ Puppet/webpack.config.js
@@ -0,0 +1,29 @@
+﻿var path = require('path')
+var webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/app.js'),
+  output: {
+    path: path.resolve(__dirname, './public'),
+    publicPath: '/',
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+	  {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  plugins: [
+	new VueLoaderPlugin()
+  ]
+}
